a7dbb0a85e47e8467874df459dda047bf45a34d1
ntl-arslan <arslanali.irshad@nayatel.com>
2025-05-22 18:20:52 +0500

    create stack dto

--- src/stacks/dto/create-stack.dto.ts
+++ src/stacks/dto/create-stack.dto.ts
@@ -1,1 +1,18 @@
-export class CreateStackDto {}
+// src/stacks/dto/create-stack.dto.ts
+
+import { IsNotEmpty, IsOptional, IsString, MaxLength } from 'class-validator';
+
+export class CreateStackDto {
+  @IsNotEmpty()
+  @IsString()
+  @MaxLength(100)
+  name: string;
+
+  @IsOptional()
+  @IsString()
+  description?: string;
+
+  @IsOptional()
+  @IsString()
+  status?: string; // 'active' | 'disabled'
+}
