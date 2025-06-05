90e9c1880644c94eacb36f2e70455e7e8c9c892e
ntl-arslan <arslanali.irshad@nayatel.com>
2025-06-05 10:56:54 +0500

    unused imports removed

--- src/questions/questions.controller.ts
+++ src/questions/questions.controller.ts
@@ -1,6 +1,5 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { QuestionsService } from './questions.service';
-import { CreateQuestionDto } from './dto/create-question.dto';
 
 
 @Controller('questions')
