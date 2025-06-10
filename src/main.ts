468c4a7a55d2ddb9d641bea612913e96ce894c8c
ntl-arslan <arslanali.irshad@nayatel.com>
2025-06-10 17:15:07 +0500

    Cross Origin Updated

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,14 @@ async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 app.use(cookieParser());
 	// Enable CORS
-	app.enableCors({
-		origin: '*',
-		methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-		credentials: true,
-	});
+app.enableCors({
+  origin: [
+    'https://quiz-app-rust-xi.vercel.app',
+    'http://localhost:3000',
+  ],
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true
+});
 
 	await app.listen(3000);
 }
